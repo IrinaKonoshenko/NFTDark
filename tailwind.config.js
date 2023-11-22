6a41f3107588b34154d48373d1977ac727a8ac40
Irina Konoshenko <ayren.syssoyeva@gmail.com>
2023-11-23 03:34:52 +0600

    add components event

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ['./markup/**/*.html'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                event: '#3D2942',
+                timer: '#8613A5',
+                'white-100': '#F5FBF2',
+                'violet-light': '#BDAAC1',
+                primary: '#FFCE4E',
+            },
+            fontFamily: {
+                oxanium: ['Oxanium', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
